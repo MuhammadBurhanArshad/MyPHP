9b7b7363a3de0cb0d7574bd48e7c2f27bce310ac
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2025-05-13 14:24:38 +0500

    Echo Statement Example

--- 04 Echo & Print Statement in PHP/index.php
+++ 04 Echo & Print Statement in PHP/index.php
@@ -1,5 +1,17 @@
 <?php
 
+echo "<h1>Hello World</h1>";
+
+echo "Hello World <br />";
+
+echo "Hello ", "World";
+
+echo "Hello ". "World";
+
+echo "Hello ". "World";
+
+echo 504;
+
 echo "This ", "is ", "a ", "test."; // correct
 
 echo '<br>';
