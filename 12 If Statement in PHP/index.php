dc7bd4b00ba6a5af0f677e27e6fb03bc827c811f
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2025-05-27 11:49:29 +0500

    If Statement in PHP

--- 12 If Statement in PHP/index.php
+++ 12 If Statement in PHP/index.php
@@ -6,18 +6,26 @@ $b = 5;
 
 if ($a > $b) {
     echo "a is greater than b";
-} elseif ($a < $b) {
+} 
+
+if ($a < $b) {
     echo "a is less than b";
-} else if ($a == $b) {
+}
+
+if ($a == $b) {
     echo "a and b are equal";
-} else if($a === $b) {
+}
+
+if($a === $b) {
     echo "a is identical to b";
-} elseif ($a != $b) {
+} 
+
+if ($a != $b) {
     echo "a is not equal to b";
-} elseif ($a !== $b) {
+} 
+
+if ($a !== $b) {
     echo "a is not identical to b";
-} else {
-    echo "a is equal to b";
 }
 
 echo "after if statement";
