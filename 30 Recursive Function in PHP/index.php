c7716b1139262c14eb8af108e006566a8d27d339
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2025-06-06 15:25:12 +0500

    Recursive Function

--- 30 Recursive Function in PHP/index.php
+++ 30 Recursive Function in PHP/index.php
@@ -1,14 +1,24 @@
 <?php 
 
-
-// this function it recursive function it will call it self by updated value each time
+// Recursive display function: prints numbers from 1 to 5
 function display($number){
     if($number <= 5) {
         echo "$number <br />";
-        display($number++);
+        display($number + 1); // or use ++$number
     }
 }
 
-// calling display function with initial value 
+// Calling display function
 display(1);
 
+// Recursive factorial function
+function factorial($number) {
+    if($number == 0) {
+        return 1;
+    } else {
+        return ($number * factorial($number - 1));
+    }
+}
+
+// Display factorial result
+echo "Factorial of 5 is: " . factorial(5);
