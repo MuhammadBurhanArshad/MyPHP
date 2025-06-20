c7de52c7a7069efc05d5dbb6ebfd2fa09d81afef
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2025-06-20 15:11:39 +0500

    array intersect functions

--- 47 Array Intersect Functions in PHP/index.php
+++ 47 Array Intersect Functions in PHP/index.php
@@ -6,15 +6,24 @@ $a2 = ["a" => "red", "f" => "green", "d" => "purple"];
 
 $newArray = array_intersect($a1, $a2);
 
+
+echo "<pre>";
 print_r($newArray); // it will match the value by case sensitive and return the intersected array, i match the value from all the given array (two or more)
+echo "</pre>";
+
 
 $newArray = array_intersect_key($a1, $a2);
 
+echo "<pre>";
 print_r($newArray); // it will match the key by case sensitive and return the intersected array and the matched key value should be from first array, i match the value from all the given array (two or more)
+echo "</pre>";
+
 
 $newArray = array_intersect_assoc($a1, $a2);
 
+echo "<pre>";
 print_r($newArray); // it will match the key and value both by case sensitive and return the intersected array, i match the value from all the given array (two or more)
+echo "</pre>";
 
 
 function compare($a, $b){
@@ -27,21 +36,35 @@ function compare($a, $b){
 
 $newArray = array_intersect_uassoc($a1, $a2, "compare");
 
+echo "<pre>";
 print_r($newArray); // it will match the key and value both by the uer defined function
+echo "</pre>";
+
 
 $newArray = array_uintersect_assoc($a1, $a2, "compare");
 
+echo "<pre>";
 print_r($newArray); // work as like array_intersect_uassoc()
+echo "</pre>";
+
 
 $newArray = array_intersect_ukey($a1, $a2, "compare");
 
+echo "<pre>";
 print_r($newArray); // also uses the use defined function to compare the keys of arrays
+echo "</pre>";
+
 
 $newArray = array_uintersect($a1, $a2, "compare");
 
+echo "<pre>";
 print_r($newArray); // also uses the use defined function to compare the values of arrays
+echo "</pre>";
+
 
 $newArray = array_uintersect_uassoc($a1, $a2, "compare", "compare"); // the first function is for comparing values and the second one is for compare keys 
 
+echo "<pre>";
 print_r($newArray); // also uses the use defined function to compare the values of arrays
+echo "</pre>";
 
